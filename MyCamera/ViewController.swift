5030621a879cd31dc02bda43be2a2bd9a468184b
NEKOTENI <riverfield.tk@gmail.com>
2017-09-06 17:45:45 +0900

    my original code

--- MyCamera/ViewController.swift
+++ MyCamera/ViewController.swift
@@ -5,21 +5,20 @@
 //  Created by T K on 2017/08/27.
 //  Copyright © 2017年 nekoteni.fun. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBOutlet weak var photoImage: UIImageView!
     
     @IBAction func cameraLaunchAction(_ sender: Any) {
@@ -49,4 +48,3 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         dismiss(animated: true, completion: nil)
     }
 }
-
